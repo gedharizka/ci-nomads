6bfb7520e5479a80f2d022596f51ba1f1a8d2bf5
Gedha Rizka <gedharizka@gmail.com>
2025-03-09 22:54:50 +0700

    feat: update year

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -46,7 +46,7 @@
       class="row border-top justify-content-center align-items-center pt-4"
     >
       <div class="col-auto text-gray-500 font-weight-light">
-        2019 Copyright Nomads • All rights reserved • Made in Jakarta
+        2025 Copyright Nomads • All rights reserved • Made in Jakarta
       </div>
     </div>
   </div>
